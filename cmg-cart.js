616e9bebfeed4a1361c5f43ce46ef03c5e1a171b
craftymousegifts <craftymousegifts@gmail.com>
2026-06-16 17:57:07 +0100

    update cmg-cart for free delivery over £30

--- cmg-cart.js
+++ cmg-cart.js
@@ -205,7 +205,8 @@ async function cmgCheckout() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        items: cart.map(i => ({ price_id: i.id, quantity: i.qty }))
+        items: cart.map(i => ({ price_id: i.id, quantity: i.qty })),
+        subtotal: cartTotal(),
       }),
     });
 
